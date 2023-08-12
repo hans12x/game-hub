8ddad055c87bc263be374be993616c70c8d22fe8
liuquan <hansxyz08@gmail.com>
2023-08-12 15:05:34 +0800

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,27 @@
+import { HStack, List, ListItem, Text } from "@chakra-ui/layout";
 import useGenres from "../hooks/useGenres";
+import { Image } from "@chakra-ui/image";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={5}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize={'lg'}>
+                {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
